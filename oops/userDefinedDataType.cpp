ecd6f6a0699b785ba34f867980e04aa2a17d551d
byteom <123omsinghpipariya@gmail.com>
2024-12-28 00:16:34 +0530

    first oops

---
+++ oops/userDefinedDataType.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Student{ // student is new data type created by me 
+    public:
+
+        string name;
+        int roll ;
+        float marks;
+
+};
+
+int main(){
+    Student x;
+    x.name="Rahul";
+    x.roll=101;
+    x.marks=90.5;
+    Student x1;
+    x1.name="samay";
+    x1.roll=10;
+    x1.marks=9.5;
+    cout<<x.name<<endl;
+}
